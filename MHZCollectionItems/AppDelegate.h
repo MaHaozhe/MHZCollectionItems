fbf832e0ac6783606520356a0434205bc38dd4a2
MaHaoZhe <411408809@qq.com>
2018-04-24 14:29:00 +0800

    first commit

---
+++ MHZCollectionItems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHZCollectionItems
+//
+//  Created by MaHaoZhe on 2018/4/24.
+//  Copyright © 2018年 HachiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHZCollectionItems/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHZCollectionItems
+//
+//  Created by MaHaoZhe on 2018/4/24.
+//  Copyright © 2018年 HachiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
