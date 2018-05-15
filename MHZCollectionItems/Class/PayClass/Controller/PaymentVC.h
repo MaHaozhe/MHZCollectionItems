d18c2c7f4ba1a01106cc3f9f49e79131a867f368
MaHaoZhe <411408809@qq.com>
2018-05-15 14:29:08 +0800

    不完整的支付宝支付

---
+++ MHZCollectionItems/Class/PayClass/Controller/PaymentVC.h
@@ -0,0 +1,13 @@
+//
+//  PaymentVC.h
+//  MHZCollectionItems
+//
+//  Created by MaHaoZhe on 2018/5/11.
+//  Copyright © 2018年 HachiTech. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PaymentVC : BaseViewController
+
+@end
---
+++ MHZCollectionItems/Class/PayClass/Controller/ZFBPaymentVC.h
@@ -0,0 +1,13 @@
+//
+//  ZFBPaymentVC.h
+//  MHZCollectionItems
+//
+//  Created by MaHaoZhe on 2018/5/11.
+//  Copyright © 2018年 HachiTech. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ZFBPaymentVC : BaseViewController
+
+@end
